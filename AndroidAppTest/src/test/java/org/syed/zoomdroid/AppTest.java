abaefd45ba2e48c7ef8b2579073a398476b3ba6d
salk7 <syeds.248@gmailcom>
2015-03-19 23:57:31 +0530

    minor bugs fixed

--- AndroidAppTest/src/test/java/org/syed/zoomdroid/AppTest.java
+++ AndroidAppTest/src/test/java/org/syed/zoomdroid/AppTest.java
@@ -27,7 +27,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class App10est {
+public class AppTest {
 	AndroidDriver driver;
 	Process appium ;
 	@BeforeTest
