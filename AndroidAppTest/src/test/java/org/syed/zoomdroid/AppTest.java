9aa874cb99f4c51a92c45159a8aec02a9d7d2a46
salk7 <syeds.248@gmailcom>
2015-03-20 12:19:18 +0530

    appium fixed with log

--- AndroidAppTest/src/test/java/org/syed/zoomdroid/AppTest.java
+++ AndroidAppTest/src/test/java/org/syed/zoomdroid/AppTest.java
@@ -1,10 +1,10 @@
 package org.syed.zoomdroid;
+
 //import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -23,21 +24,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class AppTest {
 	AndroidDriver driver;
-	Process appium ;
+	Process Proc = null;
+
+	public void startAppiumServer() throws IOException, InterruptedException {
+
+		String[] command = { "cmd.exe","/c", "node E:\\Software\\Appium\\node_modules\\appium" };
+		ProcessBuilder probuilder = new ProcessBuilder(command);
+		probuilder.redirectOutput(new File("./AppiumLog.txt"));
+		Proc = probuilder.start();
+
+		// Proc =
+		// Runtime.getRuntime().exec("cmd.exe node E:\\Software\\Appium\\node_modules\\appium");
+		System.out
+				.println("------------------Appium server started-------------------");
+		Thread.sleep(5000);
+	}
+
 	@BeforeTest
-	public void setUp() throws IOException {try
-	{		appium= Runtime.getRuntime().exec("cmd node E:\\Software\\Appium\\node_modules\\appium");
-			Thread.sleep(10000);
-	System.out.println("------------------Appium server started-------------------");
-	
+	public void setUp() throws IOException, InterruptedException {
+	try{
+		startAppiumServer();
 		System.out.println("-------------Test Started-----------------");
-		try{
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("deviceName", "XiomiHM Note");
 		capabilities.setCapability(CapabilityType.VERSION, "4.4.2");
@@ -48,27 +61,25 @@ public class AppTest {
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
 				capabilities);
 		driver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
-		}
-		catch (Exception e)
-		{
-//			appium.destroy();
-			System.out.println(e.getMessage());
-		}
-		}
-	catch(Exception e)
-	{
-		System.out.println(e.getMessage());
-		appium.destroy();
-		System.out.println("exit no :"+appium.exitValue());
 	}
+	catch(Exception e){
+		System.out.println(e.getMessage());
+		System.out.println("Process destroyed");
+		Proc.destroy();
+		
+	}
+//		System.out.println("...............Test service initialized...............");
 	}
 
 	public String[] lessThan24() {
-//		driver.startActivity("com.zoomcar", "com.zoomcar.activity.","com.zoomcar",driver.currentActivity());
-		String eDateTime,sDateTime ;
-		System.out.println("-------------Less than 24hrs date picker------------- ");
+		// driver.startActivity("com.zoomcar",
+		// "com.zoomcar.activity.","com.zoomcar",driver.currentActivity());
+		String eDateTime, sDateTime;
+		System.out
+				.println("-------------Less than 24hrs date picker------------- ");
 		SimpleDateFormat sdf = new SimpleDateFormat("MMddhhmma");
-		GregorianCalendar cal = (GregorianCalendar) GregorianCalendar.getInstance();
+		GregorianCalendar cal = (GregorianCalendar) GregorianCalendar
+				.getInstance();
 		int min = cal.get(GregorianCalendar.MINUTE);
 		int curMin = min;
 		Date date = cal.getTime();
@@ -88,69 +99,77 @@ public class AppTest {
 		sDateTime = sdf.format(date);
 		System.out.println("start time \t:\t" + date);
 		Random x;
-		do{
-		x= new Random();
-		cal.add(GregorianCalendar.DATE, x.nextInt(30));
+		do {
+			x = new Random();
+			cal.add(GregorianCalendar.DATE, x.nextInt(30));
 
-//		cal.add(GregorianCalendar.DATE, 13);
-		
-		date = cal.getTime();
-		System.out.println("end time \t:\t" + date);
-		eDateTime = sdf.format(date);
-		}while(sDateTime.equals(eDateTime));
-				String dates[]={curDate,sDateTime,eDateTime};
+			// cal.add(GregorianCalendar.DATE, 13);
+
+			date = cal.getTime();
+			System.out.println("end time \t:\t" + date);
+			eDateTime = sdf.format(date);
+		} while (sDateTime.equals(eDateTime));
+		String dates[] = { curDate, sDateTime, eDateTime };
 		return dates;
 	}
 
-	 @Test(priority = 1, enabled = true)
+	@Test(priority = 1, enabled = true)
 	public void within24Booking() throws InterruptedException {
-		try{
+		try {
 			swipeLR();
-		 driver.findElement(By.id("com.zoomcar:id/textViewSearch")).click();
-		 }
-		catch(Exception e)
-		{
+			driver.findElement(By.id("com.zoomcar:id/textViewSearch")).click();
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		 String dates[]=lessThan24();
-		 String curDate=dates[0],sDateTime=dates[1],eDateTime=dates[2] ;
-		 boolean flag=false;
+		String dates[] = lessThan24();
+		String curDate = dates[0], sDateTime = dates[1], eDateTime = dates[2];
+		boolean flag = false;
 		int curMonth = Integer.parseInt(curDate.substring(0, 2));
 		String sMonth = sDateTime.substring(0, 2);
 		int smon = Integer.parseInt(sMonth);
 		if (smon > curMonth) {
 			System.out.println("selecting month");
 			for (int i = 0; i < (smon - curMonth); i++)
-				driver.findElement(By.id("com.zoomcar:id/calendar_right_arrow")).click();
+				driver.findElement(By.id("com.zoomcar:id/calendar_right_arrow"))
+						.click();
 		}
-		String sDay = String.valueOf(Integer.parseInt(sDateTime.substring(2, 4)));
-		if(Integer.parseInt(sDay)>24)flag=true;else flag=false;
+		String sDay = String
+				.valueOf(Integer.parseInt(sDateTime.substring(2, 4)));
+		if (Integer.parseInt(sDay) > 24)
+			flag = true;
+		else
+			flag = false;
 		String sHour = sDateTime.substring(4, 6);
 		String sMin = sDateTime.substring(6, 8);
 		String ampm = sDateTime.substring(8, 10).toUpperCase();
-		listElements("com.zoomcar:id/calendar_tv", sDay,flag);
-//		driver.manage().timeouts().implicitlyWait(60L, TimeUnit.SECONDS);
+		listElements("com.zoomcar:id/calendar_tv", sDay, flag);
+		// driver.manage().timeouts().implicitlyWait(60L, TimeUnit.SECONDS);
 		driver.findElement(By.name(sHour)).click();
 		driver.findElement(By.id("com.zoomcar:id/textView" + ampm)).click();
 		driver.findElement(By.id("com.zoomcar:id/textView" + sMin)).click();
-//		---------------------------------------------------------------------
+		// ---------------------------------------------------------------------
 		String eMonth = eDateTime.substring(0, 2);
 		int emon = Integer.parseInt(eMonth);
-		curMonth=Integer.parseInt(sMonth);
+		curMonth = Integer.parseInt(sMonth);
 		System.out.println("curMonth: " + curMonth);
-		System.out.println("emon : "+emon);
+		System.out.println("emon : " + emon);
 		if (emon > curMonth) {
 			System.out.println("selecting month");
 			for (int i = 0; i < (emon - curMonth); i++)
-				driver.findElement(By.id("com.zoomcar:id/calendar_right_arrow")).click();
+				driver.findElement(By.id("com.zoomcar:id/calendar_right_arrow"))
+						.click();
 		}
-		String eDay = String.valueOf(Integer.parseInt(eDateTime.substring(2, 4)));
-		if(Integer.parseInt(eDay)>24)flag=true; else flag=false;
+		String eDay = String
+				.valueOf(Integer.parseInt(eDateTime.substring(2, 4)));
+		if (Integer.parseInt(eDay) > 24)
+			flag = true;
+		else
+			flag = false;
 		String eHour = eDateTime.substring(4, 6);
 		String eMin = eDateTime.substring(6, 8);
 		ampm = eDateTime.substring(8, 10).toUpperCase();
-		listElements("com.zoomcar:id/calendar_tv", eDay,false);
-//		driver.manage().timeouts().implicitlyWait(60L, TimeUnit.SECONDS);
+		listElements("com.zoomcar:id/calendar_tv", eDay, false);
+		// driver.manage().timeouts().implicitlyWait(60L, TimeUnit.SECONDS);
 		driver.findElement(By.name(eHour)).click();
 		driver.findElement(By.id("com.zoomcar:id/textView" + ampm)).click();
 		driver.findElement(By.id("com.zoomcar:id/textView" + eMin)).click();
@@ -160,60 +179,65 @@ public class AppTest {
 		driver.findElement(By.id("android:id/up")).click();
 	}
 
-	@Test(enabled=true)
+	@Test(enabled = true)
 	public void locationName() throws Exception {
-		
+
 		WebElement LocMenu;
-		String cities[]={"Bangalore","Pune","Delhi NCR"};
-		String locText=null;
+		String cities[] = { "Bangalore", "Pune", "Delhi NCR" };
+		String locText = null;
 		clickonHome();
-		LocMenu = driver.findElement(By.id("com.zoomcar:id/textViewCityLocation"));
+		LocMenu = driver.findElement(By
+				.id("com.zoomcar:id/textViewCityLocation"));
 		for (int i = 0; i < cities.length; i++) {
 			LocMenu.click();
-			listElements("com.zoomcar:id/textViewLocationName",cities[i],false);
+			listElements("com.zoomcar:id/textViewLocationName", cities[i],
+					false);
 			swipeLR();
 			Thread.sleep(2000);
-			locText=LocMenu.getText();
-			Assert.assertEquals(locText,cities[i] );
-//			continue;
+			locText = LocMenu.getText();
+			Assert.assertEquals(locText, cities[i]);
+			// continue;
 		}
 		clickonHome();
 	}
-	
-	public void swipeLR() throws InterruptedException
-	{
+
+	public void swipeLR() throws InterruptedException {
 		Thread.sleep(1000);
 		Dimension size = driver.manage().window().getSize();
 		int endx = (int) (size.width * 0.8);
 		int startx = (int) (size.width * 0.05);
-		int starty = size.height / 2; 
+		int starty = size.height / 2;
 		driver.swipe(startx, starty, endx, starty, 200);
-		System.out.println("swipeRight");	
+		System.out.println("swipeRight");
 		Thread.sleep(1000);
-//		driver.swipe(10, 900, 576, 900, 500);
+		// driver.swipe(10, 900, 576, 900, 500);
 	}
-	public void listElements(String parentElement, String childElement, boolean lastElement) {
+
+	public void listElements(String parentElement, String childElement,
+			boolean lastElement) {
 		List<WebElement> weList = null;
-		WebElement el=null;
+		WebElement el = null;
 		weList = driver.findElements(By.id(parentElement));
 		for (WebElement we : weList) {
 			// System.out.println("listing elements"+we.getText());
-			if (we.getText().toLowerCase().equals(childElement.toLowerCase()) ){
-				
-				el=we;
-				if(lastElement==false)break;
+			if (we.getText().toLowerCase().equals(childElement.toLowerCase())) {
+
+				el = we;
+				if (lastElement == false)
+					break;
 			}
 		}
 		System.out.println("clicking on :" + el.getText());
 		el.click();
 	}
-//	@Test()
+
+	// @Test()
 	public void screenshot() throws InterruptedException, IOException {
 		driver.findElement(By.id("android:id/home")).click();
 		// driver.findElement(By.id("com.zoomcar:id/textViewMenuParent")).;
 		// List<WebElement> weList;
-		listElements("com.zoomcar:id/textViewMenuParent", "how it works",false);
-		listElements("com.zoomcar:id/textViewMenuChild", "faqs",false);
+		listElements("com.zoomcar:id/textViewMenuParent", "how it works", false);
+		listElements("com.zoomcar:id/textViewMenuChild", "faqs", false);
 		File srcFile = ((TakesScreenshot) driver)
 				.getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(srcFile, new File("./screenshots/fname.png"));
@@ -223,14 +247,15 @@ public class AppTest {
 
 	}
 
-	@AfterTest
+	@AfterClass
 	public void tearDown() throws InterruptedException {
-		if(driver!=null)
-		driver.quit();
+		if (driver != null)
+			driver.quit();
 		System.out.println("---------------------End of test--------------");
-		if(appium!=null)
-		appium.destroy();
-		System.out.println("------------appium server destroyed------------------");
-//		Thread.sleep(10000);
+		if (Proc != null)
+			Proc.destroy();
+		System.out
+				.println("------------appium server destroyed------------------");
+		// Thread.sleep(10000);
 	}
 }
